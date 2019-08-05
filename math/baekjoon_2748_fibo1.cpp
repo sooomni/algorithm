95684cb81149ce9b8d6f8a3bb6454a21bb58a424
Soomin Kim <sooomni@github.com>
2019-08-05 09:39:23 +0900

    DP

---
+++ math/baekjoon_2748_fibo1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int fibo(int n);
+
+int main(void) {
+	int n = 0;
+	cin >> n;
+
+	if (n > 90) {
+		return -1;
+	}
+	
+	cout << fibo(n);
+	
+	return 0;
+}
+
+int fibo(int n) {
+	int * p = new int[n];
+
+	p[0] = 0;
+	p[1] = 1;
+	p[2] = 1;
+
+	for(int i = 3;i<=n;i++){
+		p[i] = p[i - 1] + p[i - 2];
+	}
+
+	return p[n];
+}
---
+++ math/baekjoon_2748_fibo2.cpp
@@ -0,0 +1,36 @@
+﻿#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int fibo(int n,int * p);
+
+int main(void) {
+	int n = 0;
+	cin >> n;
+
+	if (n > 90) {
+		return -1;
+	}
+
+	int * p = new int[n];
+	fill_n(p, n, -1);
+	cout << fibo(n,p);
+
+	return 0;
+}
+
+int fibo(int n,int * p) {
+
+	if (n == 0)
+		return 0;
+	else if (n == 1 || n == 2)
+		return 1;
+	else if (p[n] > -1)
+		return p[n];
+	else {
+		p[n] = fibo(n-1,p) + fibo(n - 2,p);
+		return p[n];
+	}
+}
