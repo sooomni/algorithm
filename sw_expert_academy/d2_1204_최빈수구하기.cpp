72086e1dc3043c9dd47ece355f2c8fdccc4452af
Soomin Kim <sooomni@github.com>
2019-09-22 01:53:22 +0900

    2019-09-22

---
+++ sw_expert_academy/d2_1204_최빈수구하기.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void print_result(int num, int * test_case);
+int func(int * test_case);
+
+int main(void) {
+	int test_case;
+	int p_test_case = 1;
+
+	cin >> test_case;
+
+	while (p_test_case<=test_case) {
+
+		print_result(func(&p_test_case), &p_test_case);
+		p_test_case++;
+	}
+
+	return 0;
+}
+
+void print_result(int num, int * p_test_case) {
+	cout << "#" <<*p_test_case << " ";
+	cout << num<<endl;
+}
+
+int func(int * p_test_case) {
+	
+	int * arr = new int[101]();
+	int num = 0;
+	int n;
+	int max = 0;
+	int max_index;
+
+	for (int i = 0; i <= 1000; i++) {
+		cin >> n;
+		arr[n]++;
+	}
+
+	max = arr[0];
+	max_index = 0;
+    
+    for (int i = 0; i <= 100; i++) {
+		if (max == arr[i]) {
+            max_index = i;
+        }
+        else if(max < arr[i]){
+			max = arr[i];
+			max_index = i;
+    }
+}
+	delete[] arr;
+
+	return max_index;
+}
---
+++ sw_expert_academy/d3_1206_view.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void print_result(int num, int * test_case);
+int func(int * test_case);
+
+int main(void) {
+	int p_test_case = 1;
+
+	while (p_test_case <= 10) {
+
+		print_result(func(&p_test_case), &p_test_case);
+		p_test_case++;
+	}
+
+	return 0;
+}
+
+void print_result(int num, int * p_test_case) {
+	cout << "#" << *p_test_case << " ";
+	cout << num << endl;
+}
+
+int func(int * p_test_case) {
+	
+	int test_set;
+	int n;
+	int result = 0;
+	int target;
+	int min;
+	bool check = true;
+
+	cin >> test_set;
+	
+	int * arr = new int[test_set]();
+	
+	for (int i = 0; i < test_set; i++)
+	{
+		cin >> n;
+		arr[i] = n;
+	}
+
+	for (int i = 2; i < test_set-2; i++){	
+		target = arr[i];
+		min = 255;
+		check = true;
+
+		for (int j = i - 2; j <= i + 2; j++) {
+			if (j == 0 || j == 1|| j == 999|| j == 1000||j == i) {
+				continue;
+			}
+			else if (target - arr[j] <= 0) {
+				check = false;
+				continue;
+			}
+            else if (check== true && target - arr[j] < min) {
+				min = target - arr[j];
+			}
+		}
+		if (check == true) {
+			result += min;
+		}
+	}
+
+	delete[] arr;
+
+	return result;
+}
