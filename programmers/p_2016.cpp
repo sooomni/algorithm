6d291c6f5efd6f9a63cc409ab8d26d4d7f06940e
Soomin Kim <sooomni@github.com>
2019-11-18 15:38:10 +0900

    2019-11-18

---
+++ programmers/p_2016.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+    string day[7] = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+    int month[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    int idx = 0;
+    for(int i =0;i<a-1;i++){
+        idx += month[i];    
+    }
+    idx = (idx+b-1)%7;
+    string answer = day[idx];
+    return answer;
+}
---
+++ programmers/p_Greedy.cpp
@@ -0,0 +1,37 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    int * arr = new int[n+1]();
+    
+    for(int i = 0;i<lost.size();i++){
+        arr[lost[i]]--;
+    }
+    for(int i = 0;i<reserve.size();i++){
+        arr[reserve[i]]++;
+     }
+     
+    for(int i=0;i<=n;i++){
+        if(i != 0 && arr[i-1] <0){
+            if(arr[i]>0){
+                arr[i]--;
+                arr[i-1]++;
+            }
+        }
+        else if(i != n && arr[i+1] <0){
+            if(arr[i]>0){
+                arr[i]--;
+                arr[i+1]++; 
+            }  
+        }
+    }  
+    
+    for(int i = 1;i<n+1;i++){
+        if(arr[i]>=0)
+            answer+=1;
+    }
+    return answer;
+}
