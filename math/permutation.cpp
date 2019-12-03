9e215b4bc9274e1bad1f775fe3c072ac7830ecc9
Soomin Kim <sooomni@github.com>
2019-12-03 19:18:08 +0900

    sort_create;

---
+++ math/permutation.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void perm_print(vector <int> * v) {
+	for (int i = 0; i < (*v).size(); i++) {
+		if (i != (*v).size()-1)
+			cout << (*v)[i] << " ";
+		else
+			cout << (*v)[i] << endl;
+	}
+
+}	
+void swap(vector <int>* v, int i, int j) {
+	int t = (*v)[i];
+	(*v)[i] = (*v)[j];
+	(*v)[j] = t;
+}
+
+void permutation(vector <int>* v, int dept, int n, int k) {
+	if (dept == k) {
+		perm_print(v);
+		return;
+	}
+
+	for (int i = dept; i < n; i++) {
+		swap(v, i, dept);
+		permutation(v, dept + 1, n, k);
+		swap(v, i, dept);
+	}
+
+}
+
+
+
+int main(void) { 
+	vector <int> v;
+
+	int n = 0;
+	int input = 0;
+	cin>> n;
+
+	for (int i = 0; i < n; i++) {	
+		cin >> input;
+		v.push_back(input);
+	}
+
+	permutation(&v, 0, v.size(), v.size());
+
+	return 0;
+}
+
---
+++ sort/p_H-Index.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+bool compare(int a, int b){
+    return a>b;
+}
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(),citations.end(),compare);
+    
+    for(int i = 0;i<citations.size();i++){
+        if(citations[i]<= answer) break;
+        answer ++;       
+    }
+    return answer;
+}
