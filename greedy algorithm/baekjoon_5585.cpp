6d64bb351ed96cc371e0c5ee178550c2a8fd42b6
Soomin Kim <sooomni@github.com>
2019-12-03 14:54:47 +0900

    2019-12-03

---
+++ greedy algorithm/baekjoon_5585.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main(void) {
+	int n = 0;
+	int answer = 0;
+	string str = "";
+
+	getline(cin, str);
+	n = atoi(str.c_str());
+    
+	n = 1000 - n;
+	answer += n / 500;
+	n %= 500;
+	answer += n / 100;
+	n %= 100;
+	answer += n / 50;
+	n %= 50;
+	answer += n / 10;
+	n %= 10;
+	answer += n / 5;
+	n %= 5;
+	answer += n / 1;
+
+	cout << answer;
+	return 0;
+}
