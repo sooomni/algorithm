4348dfdb174a63fb5471da526310c00f0066cbd6
Soomin Kim <sooomni@github.com>
2019-12-05 00:18:29 +0900

    2019-12-05

---
+++ programmers/p_l1_문자열_내_마음대로_정렬하기.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+bool compare(pair<char, string>a, pair<char, string>b) {
+	if (a.first == b.first) {
+		return a.second < b.second;
+	}
+	else {
+		return a.first < b.first;
+	}
+}
+vector<string> solution(vector<string> strings, int n) {
+    vector <pair<char, string>> v;
+    vector<string>answer;
+    
+    for(int i = 0;i<strings.size();i++){
+        v.push_back(pair<char, string>(strings[i][n],strings[i]));
+    }
+
+    sort(v.begin(),v.end(),compare);    
+    
+    for(int i = 0;i<strings.size();i++){
+        answer.push_back(v[i].second);
+    }
+    return answer;
+}
