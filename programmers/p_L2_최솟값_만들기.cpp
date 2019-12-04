4b1f1bab1a598cc5cb84c221304eae3c0861116b
Soomin Kim <sooomni@github.com>
2019-12-04 23:13:50 +0900

    2019-12-04

---
+++ programmers/p_L2_최솟값_만들기.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+    
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end(),greater<int>());
+    
+    int i = 0;
+    int j = B.size();
+    
+    while(i<A.size()){
+        answer += (A[i]*B[i]);        
+        i++;
+        j--;        
+    }
+    return answer;
+}
---
+++ programmers/p_L2_행렬의_곱셈.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    
+    int p = arr1.size();
+    int q = arr2[0].size();
+    int r = arr2.size();
+    
+    for(int i = 0;i<p;i++){
+        vector<int> col; 
+        for(int j = 0;j<q;j++){
+            int result = 0;
+            for(int k = 0;k<r;k++){                
+              result += (arr1[i][k]*arr2[k][j]);
+            }
+            col.push_back(result);
+            cout<<endl;
+        }
+         answer.push_back(col);        
+     }    
+              
+    return answer;
+}
