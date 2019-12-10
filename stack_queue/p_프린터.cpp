757a58d87af6bdfa1107109896303611dd2f21d8
Soomin Kim <sooomni@github.com>
2019-12-10 18:25:49 +0900

    modify_p_프린터.cpp

--- stack_queue/p_프린터.cpp
+++ stack_queue/p_프린터.cpp
@@ -1,41 +1,34 @@
-﻿#include <string>
-#include <vector>
+﻿#include <vector>
 #include <iostream>
 #include <queue>
 #include <algorithm>
 
 using namespace std;
-bool compare(pair<int, int> a, pair<int, int> b){
-    return a.second > b.second;
-}
 
 int solution(vector<int> priorities, int location) {
-    int answer = 1;
-    vector <pair<int, int>> v;
-    vector <pair<int, int>> v2;
-    int max = *max_element(priorities.begin(),priorities.end());
+    int answer = 0;
+    queue <pair<int, int>> q;
+    priority_queue <int> pq;
     
-    for(int i = 0; i<priorities.size();i++){
-        if(priorities[i] == max){
-            while(i<priorities.size()){
-                v.push_back(pair<int,int>(i,priorities[i]));
-                i++;
+    for(int i = 0;i<priorities.size();i++){
+        q.push(pair<int, int>(i,priorities[i]));
+        pq.push(priorities[i]);       
+    }
+    
+    while(!q.empty()){
+        if(pq.top() == q.front().second){
+            if(location == q.front().first){
+                return ++answer;
+            }
+            else{
+                answer++;
+                pq.pop(); q.pop();
             }
         }
-        else{  
-            v2.push_back(pair<int,int>(i,priorities[i]));
-        }            
+        else{
+            q.push(q.front());
+            q.pop();
+        }     
     }
-    
-    sort(v.begin(),v.end(),compare);
-    sort(v2.begin(),v2.end(),compare);
-  
-    v.insert( v.end(), v2.begin(), v2.end() );   
-
-    for(int i = 0; i<v.size();i++){
-        if(v[i].first == location) break;
-        else answer++;
-    }
-    
     return answer;
 }
