a4ae4f0a5a17525fe397fe9333fc7ebffdf9393d
Soomin Kim <sooomni@github.com>
2019-12-16 03:00:20 +0900

    stack queue create

---
+++ stack_queue/p_다리를_지나는_트럭.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 0;
+    vector<int> completed;
+    queue <int> in_the_bridge;
+    
+    int sum = 0;
+    int i = 0;
+    while(completed.size() < truck_weights.size()){
+        answer++;
+        
+        if(answer%bridge_length == 0){
+            sum-= in_the_bridge.front();
+            completed.push_back(in_the_bridge.front());
+            in_the_bridge.pop();
+        } 
+        
+        if(in_the_bridge.empty()){
+            in_the_bridge.push(truck_weights[i]);
+            sum += truck_weights[i];
+            i++;
+        }
+            while(sum+truck_weights[i]<=weight){
+                in_the_bridge.push(truck_weights[i]);
+                sum += truck_weights[i];
+                i++;
+            }                
+    }
+    return answer;
+}
---
+++ stack_queue/p_주식가격.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    stack <int> block;
+    
+    for(int i = 0;i<prices.size();i++){
+        block.push(prices[i]);
+    }
+    
+    int tmp = 10001;
+    int i = prices.size();
+    
+    while(!block.empty()){
+        if(tmp < block.top()){
+            answer.push_back(1);
+        }
+        else{
+            answer.push_back(prices.size()-i);    
+        }
+        
+        tmp = block.top();
+        block.pop();
+        i--;
+    }
+    
+    reverse(answer.begin(),answer.end());
+    return answer;
+}
