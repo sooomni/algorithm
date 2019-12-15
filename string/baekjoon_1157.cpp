864ec4f1bc5410b1a2dc7c752b733fb03530b4d0
Soomin Kim <sooomni@github.com>
2019-12-16 02:59:56 +0900

    2019-12-16

---
+++ string/baekjoon_1157.cpp
@@ -0,0 +1,43 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool compare(pair<int, int> a, pair<int, int> b) {
+	return a.first > b.first;
+}
+
+void solution(string s) {
+	int arr[27];
+	fill_n(arr, 27, 0);
+
+	for (int i = 0; i < s.size(); i++) {
+		if ((s[i] - 'A') >= 32)
+			arr[(s[i] - 'A') % 32]++;
+		else
+			arr[(s[i] - 'A')]++;
+	}
+
+	vector<pair<int, int>> v;
+	int index = 0;
+
+	for (int i = 0; i < 27; i++) {
+		v.push_back(pair<int, int>(arr[i], i));
+	}
+
+	sort(v.begin(), v.end(), compare);
+
+	if (v[0].first == v[1].first)
+		cout << "?" << endl;
+	else 
+		cout << static_cast<char>(v[0].second+'A') << endl;
+}
+
+int main(void) {
+	string s = "";
+	cin >> s;
+	solution(s);
+
+	return 0;
+}
---
+++ string/baekjoon_2908.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void solution(string a, string b) {
+	reverse(a.begin(), a.end());
+	reverse(b.begin(), b.end());
+
+	if (a > b)
+		cout << a << endl;
+	else
+		cout << b << endl;
+}
+
+int main(void) {
+	string a = "", b = "";
+	cin >> a >> b;
+	solution(a,b);
+
+	return 0;
+}
