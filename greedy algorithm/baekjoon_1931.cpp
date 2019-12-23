24cd46013dbb37bc257ba0778147c55766b1d84f
Soomin Kim <sooomni@github.com>
2019-12-23 19:51:09 +0900

    2019-12-23

---
+++ greedy algorithm/baekjoon_1931.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Time {
+	int begin;
+	int end;
+};
+
+bool compare(Time a, Time b) {
+	if (a.end == b.end) return a.begin<b.begin;
+	else return a.end < b.end;
+}
+
+int solution(vector <Time> v) {
+	int answer = 0;
+	sort(v.begin(), v.end(), compare);
+	
+	int endpoint = 0;
+	for (int i = 0; i < v.size(); i++) {
+		if (endpoint <= v[i].begin) {
+			endpoint = v[i].end;
+			answer++;
+		}
+	}
+	return answer;
+}
+
+
+int main(void) {
+	
+	int n = 0;
+	cin >> n;
+	vector <Time> v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin>>v[i].begin >> v[i].end;
+	}
+	
+	cout<<solution(v)<<endl;
+	return 0;
+}
---
+++ string/baekjoon_5622.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+void solution(string s) {
+	int times[27] = { 3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10 };
+	int sum = 0;
+	for (int i = 0; i < s.size(); i++) {
+		int index = (s[i] - 'A');
+		sum += times[index];
+	}
+    cout<<sum<<endl;
+}
+int main(void) {
+	string s = "";
+	cin >> s;
+	solution(s);
+
+	return 0;
+}
