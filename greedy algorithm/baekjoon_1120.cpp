59d22391cb944d032efd18728241fbf116443550
Soomin Kim <sooomni@github.com>
2020-01-03 17:14:49 +0900

    2020-01-03

---
+++ greedy algorithm/baekjoon_1120.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+
+int solution(string * a,string * b) {
+	int ans = 0;
+	while (a->size() < b->size()) {
+		int cha = b->size() - a->size();
+		int front = 0, back = 0;
+		for (int i = 0; i < a->size(); i++) {
+			if (a->at(i) != b->at(i)) front++;
+			if (a->at(i) != b->at(i + cha)) back++;
+		}
+		
+		if (front == 0 || back == 0)
+			return ans = 0;
+
+		else if (front > back) {
+			*a += b->at(a->size());
+			ans += back;
+		}
+		else {
+			string t = "";
+			t = b->at(a->size());
+			t += *a;
+			*a = t;
+			ans += front;
+		}
+	}
+
+	return ans;
+}
+int main(void) {
+	int answer = 0;
+	string a = "";
+	string b = "";
+
+	cin >> a >> b;
+	int cha = b.size() - a.size();
+	if (cha == 0) {
+		for (int i = 0; i < a.size(); i++) {
+			if (a[i] != b[i]) answer++;
+		}
+		cout << answer << endl;
+	}	
+	else
+		cout<<solution(&a, &b)<<endl;
+	
+	return 0;
+}
---
+++ greedy algorithm/baekjoon_2875.cpp
@@ -0,0 +1,41 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int m, int k) {
+	int answer = 0;
+	
+	while (n > 0 && m > 0) {
+		if (n - 2 < 0 || m - 1 < 0) break;
+		n -= 2;
+		m -= 1;
+		answer += 1;
+	}
+	
+	while (k > 0) {
+		if (m <= 0 && n <= 0) {
+			answer -= 1;
+			n += 2;
+			m += 1;
+		}
+		if (m > 0) {
+			m--; k--;
+		}		
+		if (n > 0) {
+			n--; k--;
+		}			
+	}
+
+	return answer;
+}
+int main(void){
+	int n = 0, m = 0, k = 0;
+	cin >> n >> m >> k;
+	
+	if (n < 0 || n >100 || m < 0 || m>100 || k<0 || k>m + n)
+		return -1;
+
+	cout<<solution(n,m,k)<<endl;
+	return 0;
+}
